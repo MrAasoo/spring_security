7236475206edb0a9646e7ee8c821e48cc34cf846
Himanshu <himanshu1061999@gmail.com>
2025-05-31 01:58:04 +0530

    Basic auth db user authentication

---
+++ basic_auth_db_user/src/test/java/com/aasoo/basic_auth_db_user/BasicAuthDbUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasoo.basic_auth_db_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthDbUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
